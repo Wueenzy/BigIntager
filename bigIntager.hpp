02f0236534e7018816d1b41fd9ac8b6fae0942c9
Harun Koç <82876421+Wueenzy@users.noreply.github.com>
2024-10-16 23:38:39 +0300

    BigIntager v1.1
    Divided issue

--- bigIntager.hpp
+++ bigIntager.hpp
@@ -6,13 +6,21 @@ using namespace std;
 class BigIntager{
 private:
     unordered_map<int,int> bigNumber;
-    int size = 0;
     bool negative = false;
+    bool afterNegative = false;
 public:
-    BigIntager(string);
+    BigIntager(string = "0");
     void writeBigIntager();
-    void plus(BigIntager&);
-    void multiple(BigIntager&);
+    void plus(BigIntager&,BigIntager&);
+    void subtract(BigIntager&,BigIntager&);
+    void divide(BigIntager&,BigIntager&);
+    void multiple(BigIntager&,BigIntager&);
+    bool biggerThan(BigIntager&,BigIntager&);
+    bool absoluteBiggerThan(BigIntager&,BigIntager&);
+    bool equal(BigIntager&,BigIntager&);
+    bool isZero();
+    int getSize();
+    string getNumber();
 };
 
 BigIntager::BigIntager(string inputNumber) {
@@ -20,40 +28,105 @@ BigIntager::BigIntager(string inputNumber) {
     if(negative){
         inputNumber.erase(0,1);
     }
-    size = inputNumber.length();
+    int size = inputNumber.length();
     for(int i = 1; i < size + 1 ; i++){
         bigNumber[i] = inputNumber[size - i] - '0';
     }
 }
 
 void BigIntager::writeBigIntager() {
-    if(negative){
+    if(negative && !isZero()){
         cout << "-";
     }
-    for(int i = size ; i > 0; i--){
+    bool isZero = false;
+    int zeroCount = 0;
+
+    for(int i = getSize() ; i > 0; i--){
+        if(zeroCount == getSize() - i && bigNumber[i] == 0 && i != 1){
+            zeroCount++;
+            continue;
+        }
         cout << bigNumber[i];
     }
 }
 
-void BigIntager::plus(BigIntager& othBigIntager) {
-    int lowSize = (size > othBigIntager.size) ? othBigIntager.size : size;
-    unordered_map<int,int> realBigNumber = (size >= othBigIntager.size) ? bigNumber : othBigIntager.bigNumber;
-    unordered_map<int,int> fakeBigNumber = (size < othBigIntager.size) ? bigNumber : othBigIntager.bigNumber;
-    for(int i = 1; i < lowSize + 1; i++){
+void BigIntager::plus(BigIntager& BigNumber, BigIntager& othBigNumber) {
+    if(BigNumber.negative != othBigNumber.negative && !afterNegative){
+        afterNegative = true;
+        subtract(BigNumber,othBigNumber);
+        return;
+    }
+    negative = BigNumber.negative;
+    int bigSize = (BigNumber.getSize() <= othBigNumber.getSize()) ? othBigNumber.getSize() : BigNumber.getSize();
+    unordered_map<int,int> realBigNumber = (absoluteBiggerThan(BigNumber, othBigNumber)) ? BigNumber.bigNumber : othBigNumber.bigNumber;
+    unordered_map<int,int> fakeBigNumber = (!absoluteBiggerThan(BigNumber, othBigNumber)) ? BigNumber.bigNumber : othBigNumber.bigNumber;
+    for(int i = 1; i < bigSize + 1; i++){
         bigNumber[i] = realBigNumber[i] + fakeBigNumber[i];
         while(bigNumber[i] > 9){
-            bigNumber[i] -= 10;
             realBigNumber[i+1] = (realBigNumber.count(i+1)) ? ++realBigNumber[i+1] : 1;
         }
     }
 }
 
-void BigIntager::multiple(BigIntager& othBigIntager) {
-    negative = (negative == othBigIntager.negative) ? false : true;
-    vector<int> result(size + othBigIntager.size, 0);
-    for (int i = size - 1; i >= 0; i--) {
-        for (int j = othBigIntager.size - 1; j >= 0; j--) {
-            int _multiple = bigNumber[size-i] * othBigIntager.bigNumber[othBigIntager.size-j];
+void BigIntager::subtract(BigIntager& BigNumber, BigIntager& othBigNumber) {
+    if(BigNumber.negative != othBigNumber.negative && !afterNegative){
+        afterNegative = true;
+        plus(BigNumber, othBigNumber);
+        return;
+    }
+    negative = absoluteBiggerThan(BigNumber, othBigNumber) ? BigNumber.negative : !BigNumber.negative;
+    int bigSize = (BigNumber.getSize() <= othBigNumber.getSize()) ? othBigNumber.getSize() : BigNumber.getSize();
+    unordered_map<int,int> realBigNumber = (absoluteBiggerThan(BigNumber, othBigNumber)) ? BigNumber.bigNumber : othBigNumber.bigNumber;
+    unordered_map<int,int> fakeBigNumber = (!absoluteBiggerThan(BigNumber, othBigNumber)) ? BigNumber.bigNumber : othBigNumber.bigNumber;
+    for(int i = 1; i < bigSize + 1; i++){
+        bigNumber[i] = realBigNumber[i] - fakeBigNumber[i];
+        if(bigNumber[i] < 0){
+            bigNumber[i] += 10;
+            realBigNumber[i+1]--;
+            for(int j = 1; j < bigSize - i; j++){
+                if(realBigNumber[i+j] < 0){
+                    realBigNumber[i+j+1]--;
+                    realBigNumber[i+j] += 10;
+                }
+            }
+        }
+    }
+}
+
+void BigIntager::divide(BigIntager& BigNumber, BigIntager& othBigNumber) {
+    BigNumber.negative = (BigNumber.negative == othBigNumber.negative) ? false : true;
+    negative = BigNumber.negative;
+
+    if(othBigNumber.isZero()){
+        throw invalid_argument("Divided 0?");
+        return;
+    }
+
+    if(absoluteBiggerThan(othBigNumber, BigNumber)){
+        bigNumber[1] = 0;
+        return;
+    }
+
+    BigIntager current = BigNumber;
+
+    int count = 0;
+    while (absoluteBiggerThan(current, othBigNumber)) {
+        count++;
+        current.subtract(current, othBigNumber);
+    }
+
+    for(int i = 1; count >= 1; i++){
+        bigNumber[i] = count%10;
+        count = count/10;
+    }
+}
+
+void BigIntager::multiple(BigIntager& BigNumber, BigIntager& othBigNumber) {
+    negative = (BigNumber.negative == othBigNumber.negative) ? false : true;
+    vector<int> result(BigNumber.getSize() + othBigNumber.getSize(), 0);
+    for (int i = BigNumber.getSize() - 1; i >= 0; i--) {
+        for (int j = othBigNumber.getSize() - 1; j >= 0; j--) {
+            int _multiple = BigNumber.bigNumber[BigNumber.getSize()-i] * othBigNumber.bigNumber[othBigNumber.getSize()-j];
             int _plus = _multiple + result[i + j + 1];
 
             result[i + j + 1] = _plus % 10;
@@ -68,8 +141,124 @@ void BigIntager::multiple(BigIntager& othBigIntager) {
         }
     }
 
-    size = resultStr.length();
+    int size = resultStr.length();
     for(int i = 1; i < size + 1 ; i++){
         bigNumber[i] = resultStr[size - i] - '0';
     }
+}
+
+bool BigIntager::biggerThan(BigIntager& BigNumber, BigIntager &othBigNumber) {
+    if(BigNumber.negative){
+        if(othBigNumber.negative){
+            int size = (BigNumber.getSize() >= othBigNumber.getSize()) ? BigNumber.getSize() : othBigNumber.getSize();
+            for(int i = size; i > 0;--i){
+                if(BigNumber.bigNumber[i] == 0 && othBigNumber.bigNumber[i] == 0){
+                    continue;
+                }
+                if(BigNumber.bigNumber[i] > othBigNumber.bigNumber[i]){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }else{
+            return false;
+        }
+    }else{
+        if(othBigNumber.negative){
+            return true;
+        }else{
+            int size = (BigNumber.getSize() >= othBigNumber.getSize()) ? BigNumber.getSize() : othBigNumber.getSize();
+            for(int i = size; i > 0;--i){
+                if(BigNumber.bigNumber[i] == 0 && othBigNumber.bigNumber[i] == 0){
+                    continue;
+                }
+                if(BigNumber.bigNumber[i] > othBigNumber.bigNumber[i]){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool BigIntager::equal(BigIntager& BigNumber, BigIntager &othBigNumber) {
+    int zeroCount = 0;
+    for(int i = BigNumber.getSize(); i > 0; i--){
+        if(zeroCount == BigNumber.getSize() - i && BigNumber.bigNumber[i] == 0 && i != 1){
+            BigNumber.bigNumber.erase(i);
+            zeroCount++;
+            continue;
+        }
+        break;
+    }
+
+    zeroCount = 0;
+    for(int i = othBigNumber.getSize(); i > 0; i--){
+        if(zeroCount == othBigNumber.getSize() - i && othBigNumber.bigNumber[i] == 0 && i != 1){
+            othBigNumber.bigNumber.erase(i);
+            zeroCount++;
+            continue;
+        }
+        break;
+    }
+
+    if(BigNumber.negative == othBigNumber.negative){
+        if(BigNumber.bigNumber == othBigNumber.bigNumber){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool BigIntager::absoluteBiggerThan(BigIntager& BigNumber, BigIntager &othBigNumber) {
+    int size = (BigNumber.getSize() >= othBigNumber.getSize()) ? BigNumber.getSize() : othBigNumber.getSize();
+    for(int i = size; i > 0;--i){
+        if(BigNumber.bigNumber[i] == 0 && othBigNumber.bigNumber[i] == 0){
+            continue;
+        }
+        if(BigNumber.bigNumber[i] > othBigNumber.bigNumber[i]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    return false;
+}
+
+bool BigIntager::isZero(){
+    int zeroCount = 0;
+    for(int i = getSize() ; i > 0; i--){
+        if(zeroCount == getSize() - i && bigNumber[i] == 0){
+            zeroCount++;
+            continue;
+        }
+        return false;
+    }
+    return true;
+}
+
+int BigIntager::getSize() {
+    return bigNumber.size();
+}
+
+string BigIntager::getNumber() {
+    string result = "";
+    int zeroCount = 0;
+    for(int i = getSize(); i > 0; i--){
+        if(zeroCount == getSize() - i && bigNumber[i] == 0 && i != 1){
+            bigNumber.erase(i);
+            zeroCount++;
+            continue;
+        }
+        break;
+    }
+
+    for(int i = getSize(); i > 0; i--){
+        result += bigNumber[i];
+    }
+
+    return result;
 }
